2a503cc7e34041882a34e5f81857998a34da326c
SCWinter259 <catungthongkhi2002@gail.com>
2023-06-23 14:44:25 -0600

    done with calculations

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
+import { useState } from "react";
 import Header from "./Components/Header/Header.js";
 import InputForm from "./Components/InvesmentInput/InputForm.js";
 import ResultTable from "./Components/InvestmentList/ResultTable.js";
 import './index.css';
 
 function App() {
+  const [yearlyData, setYearlyData] = useState([]);
+
   const calculateHandler = (userInput) => {
+    console.log("calculateHandler Executed!");
     // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
 
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
     const expectedReturn = +userInput["expected-return"] / 100;
     const duration = +userInput["duration"];
+
+    const initialSavings = +userInput["current-savings"];
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
+      const totalInterest = currentSavings - initialSavings;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         year: i + 1,
-        yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
+        yearlyInterest: yearlyInterest,
+        totalInterest: totalInterest,
         yearlyContribution: yearlyContribution
       });
     }
 
     // do something with yearlyData ...
+    setYearlyData(yearlyData);
+
+    console.log(yearlyData);
   };
 
   return (
     <div>
       <Header/>
-      <InputForm/>
+      <InputForm onCalculate={calculateHandler}/>
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <ResultTable/>
+      <ResultTable data={yearlyData}/>
     </div>
   );
 }
--- src/Components/InvesmentInput/InputForm.js
+++ src/Components/InvesmentInput/InputForm.js
@@ -1,19 +1,83 @@
 import styles from './InputForm.module.css'
 import InputField from './InputField';
 import Actions from './Actions';
+import { useState } from 'react';
 
-export default function InputForm() {
+// This component is the whole input form
+
+export default function InputForm(props) {
+    const [currentSavings, setCurrentSavings] = useState('');
+    const [yearlyContribution, setYearlyContribution] = useState('');
+    const [expectedReturn, setExpectedReturn] = useState('');
+    const [duration, setDuration] = useState('');
+
+    function currentSavingsChangeHandler(event) {
+        setCurrentSavings(event.target.value);
+    }
+
+    function yearlyContributionChangeHandler(event) {
+        setYearlyContribution(event.target.value);
+    }
+
+    function expectedReturnChangeHandler(event) {
+        setExpectedReturn(event.target.value);
+    }
+
+    function durationChangeHandler(event) {
+        setDuration(event.target.value);
+    }
+
+    function resetHandler() {
+        console.log("resetHandler Executed!");
+        setCurrentSavings('');
+        setYearlyContribution('');
+        setExpectedReturn('');
+        setDuration('');
+    }
+
+    function formSubmitHandler(event) {
+        console.log("formSubmitHandler Executed!");
+        event.preventDefault();
+
+        const data = {
+            "current-savings": currentSavings,
+            "yearly-contribution": yearlyContribution,
+            "expected-return": expectedReturn,
+            "duration": duration
+        }
+
+        console.log(data);
+        props.onCalculate(data);
+        resetHandler();
+    }
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={formSubmitHandler}>
             <div className={styles.inputGroup}>
-                <InputField id="current-savings" labelName="Current Savings ($)"/>
-                <InputField id="yearly-contribution" labelName="Yearly Savings ($)"/>
+                <InputField 
+                    id="current-savings" 
+                    labelName="Current Savings ($)"
+                    onValueChange={currentSavingsChangeHandler}
+                />
+                <InputField 
+                    id="yearly-contribution" 
+                    labelName="Yearly Savings ($)"
+                    onValueChange={yearlyContributionChangeHandler}
+                />
             </div>
             <div className={styles.inputGroup}>
-                <InputField id="expected-return" labelName="Expected Interest (%, per year)"/>
-                <InputField id="duration" labelName="Investment Duration (years)"/>
+                <InputField 
+                    id="expected-return" 
+                    labelName="Expected Interest (%, per year)"
+                    onValueChange={expectedReturnChangeHandler}
+                />
+                <InputField 
+                    id="duration" 
+                    labelName="Investment Duration (years)"
+                    onValueChange={durationChangeHandler}
+                />
             </div>
-            <Actions/>
+            <Actions onReset={resetHandler}/>
         </form>
     );
 }
